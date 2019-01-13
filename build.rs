894f2f9064308adac3f704b6e58d63ba8f68ad84
Belval <edouard@belval.org>
2019-01-13 01:03:37 -0500

    Add code to clean up after cmake

--- build.rs
+++ build.rs
@@ -3,26 +3,36 @@
 extern crate cc;
 extern crate git2;
 
+use std::env;
 use std::path::Path;
 use std::fs;
+use std::process::Command;
+
 use git2::Repository;
 
 fn main() {
+    // Cloning the repo
     let url = "https://github.com/Microsoft/SEAL.git";
     
-    println!("{}", url);
-
-    //let repo = match Repository::clone(url, "./seal") {
-    //    Ok(repo) => repo,
-    //    Err(e) => panic!("Failed to clone SEAL: {}", e),
-    //};
+    let _repo = match Repository::clone(url, "./seal") {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to clone SEAL: {}", e),
+    };
     
+    // Configuring before building
+    env::set_current_dir(Path::new("./seal"));
+    Command::new("cmake")
+            .arg("./src/")
+            .output()
+            .expect("failed to execute process");
+    env::set_current_dir(Path::new(".."));
+
+    // Build SEAL
     let mut build = cc::Build::new();
     build.cpp(true);
     build.flag_if_supported("-std=c++17");
-    build.flag_if_supported("-msha");
+    build.flag_if_supported("-march=native");
     build.flag_if_supported("-msse4.1");
-    build.flag_if_supported("-mssse3");
     let base_path = Path::new("./seal/src/seal/");
     let util_base_path = Path::new("./seal/src/seal/util/");
     add_cpp_files(&mut build, base_path);
@@ -30,6 +40,7 @@ fn main() {
     build.include("./seal/src");
     build.compile("seal");
 
+    // Cleanup
     fs::remove_dir_all("./seal");
 }
 
